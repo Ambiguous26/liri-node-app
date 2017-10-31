675e00406abcf4a440e8c1a88d9c3a8a3a1981ba
Aram <avalbaro@gmail.com>
2017-10-30 21:56:48 -0700

    added new files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'd4R0eHsO5o0yxA0xE7hCb3lT8',
+  consumer_secret: '0dJh612HttHVJziJLhwwFuucVCIxb2goKBYzvVIus911CdQdEx',
+  access_token_key: '924514309719203843-3Cfgvc3neGvwbKi9yl9RRfc7uG7RGlM',
+  access_token_secret: 'VLZr5BP4zAxrnplRmzIChYLPQhMh8EqgLKuyTw6zlc5iJ',
+}
+
+module.exports = twitterKeys;
