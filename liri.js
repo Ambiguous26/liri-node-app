a7f37ecc34afba925994e1b79cce68d2c3044945
Aram <avalbaro@gmail.com>
2017-11-04 01:01:41 -0700

    Got movie part to work

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ var keys = require ("./keys.js");
 var fs = require("fs");
 var request = require("request");
 var Twitter = require("twitter");
-// var Spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 
 var option = process.argv[2];
 var value = process.argv[3];
@@ -59,7 +59,7 @@ var spotifyThisSong = function(value) {
 		var result = data.tracks.items[0];
 		var trackInfo = "Track Title: " + result.name +
 										",Artist(s): " + result.album.artists[0].name +
-										",Preview Link: " + result.external_uris.spotify +
+										",Preview Link: " + result.external_urls.spotify +
 										",Album Name: " + result.album.name;
 		// the .split call separates every word with a "," It splits a string into an array of substrings.
 		var dataArr = trackInfo.split (",");
@@ -83,6 +83,52 @@ var spotifyThisSong = function(value) {
 };//end of spotifyThisSong variable
 
 
+// I don't think this code below is working properly.
+var movieThis = function(value) {
+	if (!value) {
+		value = 'Mr. Nobody'
+	}
+
+
+	// Pulled from inclass example
+	// Took the website and replaced what was searched with value
+	// the last part includes the API key that was provided.
+	request("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+		// this is a workaround, incase the error is undefined.
+		// if the request is successful
+		// need to understand this better
+		if (!error && response.statusCode === 200) {
+			// takes data received from a web server which is always a string and parses it in JS as an object
+			// Parse the body of the site and recover just the imdbRating
+			var result = "Title: " + JSON.parse(body).Title + 
+						"\nYear: " + JSON.parse(body).Year +
+		    			"\nIMDB Rating: " + JSON.parse(body).imdbRating +
+		    			"\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
+		    			"\nCountry: " + JSON.parse(body).Country +
+		    			"\nLanguage: " + JSON.parse(body).Language +
+		    			"\nPlot Summary: " + JSON.parse(body).Plot +
+						"\nActors: " + JSON.parse(body).Actors;
+			console.log(result);
+
+			fs.appendFile("log.txt", "\n\nmovie-this: \n" + result, function(err) {
+				if (err) {
+					return console.log(err);
+				}
+			});
+			console.log("\nThis movie was added to log.text");
+
+		}
+		else {
+			return console.log("Error occurred: " + error);
+		};
+	});
+
+};//end of movieThis variable
+
+
+
+// need to add the variable for what-to-do 
+
 
 switch (option) {
 	case "my-tweets":
@@ -93,10 +139,21 @@ switch (option) {
 		spotifyThisSong(value);
 		break;
 
+	case "movie-this":
+		movieThis(value);
+		break;
+
+// Just need to add the case and switch for do what it says.
+
+
 	default:
 		console.log("\nPlease enter one of the following:");
 		console.log("my-tweets");
 		console.log("spotify-this-song <Enter song title>");
+		console.log("movie-this <Enter movie title>");
+
+//Add the console.log for what to do
+		
 		break;
 };
 
